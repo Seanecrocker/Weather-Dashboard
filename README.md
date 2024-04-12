a6958234125f0b728b16c41b02080fcadfa4a30f
Sean <Seanecrocker@gmail.com>
2024-04-11 22:42:35 -0500

    appending elements

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Weather-Dashboard
+
+## Description
+
+This project is a simple page that shows you the tempature, humidity, and wind speed of a certain city.
+
+## Installation
+
+To to use this webpage you can simply use this deployed version of the website:
+
+To access the repository, click this link: 
+
+
+
+## Usage
+
+To use this project, follow these steps:
+
+1. Run the application
+2. Enter your information
+3. Click submit
+
+## Contributing
+
+If you would like to contribute to this project, please fork the repository and submit a pull request.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
+
+This is a basic template that you can customize to fit the needs of your project. Feel free to add more sections or details as necessary.
+
--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const apiKey = "e36b4c1f95b93d08f0cf7c2e3bb2deab";
 const formEl = document.getElementById("formEl"); //Html line 15
-const searchBtn = document.getElementById("searchBtn");
 const search = document.getElementById("search"); //Html line 18
 const resultsDiv = document.getElementById("results");
 
@@ -15,17 +14,42 @@ function apiCall(param) {
     .then(function(data) {
         console.log(data)
         const list = data.list
-        for (let index = 0; index < 5; index++) {
-            const element = list[index];
-            console.log(element)
-        }
-        })
+        const currentDay = list[0].dt_txt
+        const date = currentDay.split(` `)
+        const time = date[1]
 
+        const filteredList = list.filter((day)=>{
+            return day.dt_txt.split(" ") [1] === time;
+        });
+            filteredList.push(list[list.length-1])
+
+        console.log(filteredList)
+        for (let index = 0; index < filteredList.length; index++) {
+            const element = filteredList[index];
+            console.log(element)
+            
+            const resultsDiv = document.querySelector('#result')
+            const weatherInfo = document.createElement("div");
+
+const weatherBox = document.querySelector('.weather-box');
+
+const temperatureElement = document.createElement('p');
+const humidityElement = document.createElement('p');
+const windSpeedElement = document.createElement('p');
+
+temperatureElement.textContent = `Temperature: ${element.main.temp}`;
+humidityElement.textContent = `Humidity: ${element.main.humidity}`;
+windSpeedElement.textContent = `Wind Speed: ${element.wind.speed}`;
+
+weatherBox.appendChild(temperatureElement);
+weatherBox.appendChild(humidityElement);
+weatherBox.appendChild(windSpeedElement);
+    }
+  })
 }
 
 formEl.addEventListener(`submit`, function(event) {
     event.preventDefault(); 
     const cityName = search.value
     apiCall(cityName)
-
 })
